1c66a6e3cb4eb191529778d584396956952aac21
SlySlacker <lucythefilly1@gmail.com>
2022-06-11 18:15:51 -0400

    Added Room IDs, added "changename", added version verifer, changed boot text.

--- UndertaleModMenu/ModMain.cs
+++ UndertaleModMenu/ModMain.cs
@@ -9,27 +9,50 @@ using Memory;
 class UndertaleMod
 {
     public void killProc() => Process.GetCurrentProcess().Kill();
+    public void blank() => Console.WriteLine("");
     Logging l = new Logging();
     // all of these are doubles for some shitty reason
-    public string CurrentHpPtr = "Undertale.exe+00408950,44,10,D0,460"; // Current health pointer
-    public string MaxHpPtr = "Undertale.exe+00408950,44,10,D0,450"; // Max health pointer
-    public string EquWeapon = "Undertale.exe+19F1A5F0,44,10,700,120"; // Current Weapon 
-    public string CurrentGoldPtr = "Undertale.exe+003F9F44,0,44,10,364,400"; // Gold
-    public string CurrentLovePtr = "Undertale.exe+003F9F44,0,44,10,364,3E0"; // Love 
-    public string isInBattle = "Undertale.exe+003F9F44,0,44,10,1A8,30"; // Battle Check
-    public string ExpPtr = "Undertale.exe+003F9F44,0,44,10,364,3F0"; // EXP
-    public string EnemyOnePtr = "Undertale.exe+004099B4,48,8,50,14,5C0"; // Enemy One pointer
-    public string EnemyTwoPtr = "Undertale.exe+004099B4,48,8,50,14,5D0"; // Enemy Two pointer
+    public string CurrentHpPtr = "Undertale.exe+00408950,44,10,D0,460";                // Current health pointer
+    public string MaxHpPtr = "Undertale.exe+00408950,44,10,D0,450";                    // Max health pointer
+    public string EquWeapon = "Undertale.exe+19F1A5F0,44,10,700,120";                  // Current Weapon 
+    public string CurrentGoldPtr = "Undertale.exe+003F9F44,0,44,10,364,400";           // Gold
+    public string CurrentLovePtr = "Undertale.exe+003F9F44,0,44,10,364,3E0";           // Love 
+    public string isInBattle = "Undertale.exe+003F9F44,0,44,10,1A8,30";                // Battle Check
+    public string ExpPtr = "Undertale.exe+003F9F44,0,44,10,364,3F0";                   // EXP pointer
+    public string EnemyOnePtr = "Undertale.exe+004099B4,48,8,50,14,5C0";               // Enemy One pointer
+    public string EnemyTwoPtr = "Undertale.exe+004099B4,48,8,50,14,5D0";               // Enemy Two pointer
+    public string RoomPtr = "Undertale.exe+618EA0";                                    // Room Pointer
+    public string NamePtr = "Undertale.exe+003FC5EC,0,14,0,8,18,10,2B0";               // Name Pointer
 
     public void Cons(Mem mem)
     {
+        CheckVersion(mem);
+        WaitForPlayer(mem);
+        Double hp = mem.ReadDouble(CurrentHpPtr);
+        Double maxhp = mem.ReadDouble(MaxHpPtr);
+        Double gold = mem.ReadDouble(CurrentGoldPtr);
+        Double love = mem.ReadDouble(CurrentLovePtr);
+        Double weapon = mem.ReadDouble(EquWeapon);
+        Double battle = mem.ReadDouble(isInBattle);
+        Double exp = mem.ReadDouble(ExpPtr);
+        Double room = mem.Read2Byte(RoomPtr);
+        string name = mem.ReadString(NamePtr);
+
         l.logWrite("Successfully hooked to Undertale.exe");
+        l.logWrite($"Player Name: {name}");
+        Console.WriteLine("");
+        l.logWrite($"LOVE: {love}");
+        l.logWrite($"EXP: {exp}");
+        l.logWrite($"Room ID: {room}");
+        l.logWrite($"Gold: {gold}");
+        Console.WriteLine("");
 
         while (true) 
         {
             string? input = Console.ReadLine(); // anything that passes the input string to a method will need to have the input string filtered 
             if (input != null)
             {
+                string inputUpper = input.Trim();
                 input = input.ToLower().Trim();
                 if (input.Contains("sethp "))
                 {
@@ -63,7 +86,7 @@ class UndertaleMod
                 {
                     ReadValues(mem);
                 }
-                else if (input.Contains("setexp"))
+                else if (input.Contains("setexp "))
                 {
                     input = input.Replace("setexp ", "");
                     SetExp(mem, input);
@@ -83,6 +106,11 @@ class UndertaleMod
                 else if (input == "onehit")
                 {
                     OneHit(mem);
+                }
+                else if (input.Contains("changename "))
+                {
+                    inputUpper = inputUpper.Replace("changename ", "");
+                    SetName(mem, inputUpper);
                 }
 
                 else if (input == "help")
@@ -135,8 +163,12 @@ class UndertaleMod
             Console.WriteLine("setgold: Sets the current Gold value.");
             Console.WriteLine("Saving in game will make the value stick.");
             Console.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("changename: Sets the Players name.");
+            Console.WriteLine("Saving in game will make the name stick.");
             Console.WriteLine("");
+            Console.WriteLine("info: Shows some in-game info");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("WIP Commands:");
             Console.WriteLine("");
             Console.WriteLine("changeweapon");
@@ -169,11 +201,41 @@ class UndertaleMod
         }
     }
 
+    public void SetName(Mem mem, string Name)
+    {
+        try
+        {
+            byte[] ba = Encoding.Default.GetBytes(Name);
+            var hexString = BitConverter.ToString(ba);
+            string namehex = hexString.Replace("-", " ");
+            namehex = namehex + " 00";
+
+            if (Debugger.IsAttached)
+            {
+                l.logWrite(namehex.Length.ToString());
+                l.logWrite(namehex);
+            }
+
+            else
+            mem.WriteMemory(NamePtr, "bytes", "000000000000");
+            mem.WriteMemory(NamePtr, "bytes", namehex);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Name has been changed to '" + Name + "'");
+            Console.WriteLine("Save the game to keep the change.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        catch (Exception ex)
+        {
+            l.logWrite($"Couldn't write memory: {ex}");
+        }
+    }
+
     public void SetExp(Mem mem, string Exp)
     {
         try
         {
             mem.WriteMemory(ExpPtr, "double", Exp);
+            Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("EXP has been set to '" + Exp + "'");
             Console.ForegroundColor = ConsoleColor.White;
@@ -268,6 +330,7 @@ class UndertaleMod
         try
         {
             mem.UnfreezeValue(CurrentHpPtr);
+            Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Hp has been unfrozen with value " + mem.ReadDouble(CurrentHpPtr).ToString());
             Console.ForegroundColor = ConsoleColor.White;
@@ -284,6 +347,7 @@ class UndertaleMod
         {
             mem.WriteMemory(MaxHpPtr, "double", Hp);
             mem.FreezeValue(MaxHpPtr, "double", mem.ReadDouble(MaxHpPtr).ToString());
+            Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("MaxHp has been set to '" + Hp + "'");
             Console.ForegroundColor = ConsoleColor.White;
@@ -303,16 +367,77 @@ class UndertaleMod
         Double weapon = mem.ReadDouble(EquWeapon);
         Double battle = mem.ReadDouble(isInBattle);
         Double exp = mem.ReadDouble(ExpPtr);
+        uint room = mem.ReadUInt(RoomPtr);
+        string name = mem.ReadString(NamePtr);
         bool isBattling = false;
         if(battle > 0) isBattling = true;
+        Console.WriteLine("");
         Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"Player Name: {name}");
+        Console.WriteLine($"");
         Console.WriteLine($"Current hp: {hp}/{maxhp}");
         Console.WriteLine($"Current gold: {gold}");
+        Console.WriteLine($"Current Room ID: {room}");
+        Console.WriteLine($"");
         //Console.WriteLine($"Current weapon: {weapon}");
         Console.WriteLine($"Current love: {love}");
         Console.WriteLine($"Current EXP: {exp}");
+        Console.WriteLine($"");
         Console.WriteLine($"Is battling: {isBattling}");
+        Console.WriteLine("");
         Console.ForegroundColor = ConsoleColor.White;
     }
+
+    public void CheckVersion(Mem mem)
+    {
+
+        uint room2 = mem.ReadUInt(RoomPtr);
+        uint room1 = mem.ReadUInt("UNDERTALE.exe+50F300");
+        if (Debugger.IsAttached)
+        {
+            try
+            {
+                Console.WriteLine("1.0 roomid: " + room1);
+                Console.WriteLine("1.08 roomid: " + room2);
+            }
+            catch (Exception ex)
+            {
+                l.logWrite("idk man: " + ex, "error");
+            }
+        }
+        if (room1 > 0)
+        {
+            l.logWrite("1.0 - 1.07 detected. This version is not supported.");
+            l.logWrite("Press any key to exit..."); 
+            Console.ReadKey();
+            killProc();
+        }
+    }
+    public void WaitForPlayer(Mem mem)
+    {
+        bool msgDisplayed = false;
+        bool isInGame = false;
+        while (!isInGame)
+        {
+            uint room = mem.ReadUInt(RoomPtr);
+            if (room < 4)
+            {
+                if (!msgDisplayed) {
+                    l.logWrite("Waiting for you to load a save first...");
+                    l.logWrite("If you load a save and this doesn't continue, your version of UNDERTALE is not 1.08.");
+                    msgDisplayed = true;
+                }
+            }
+            else
+            {
+                if(msgDisplayed) l.logWrite("Continuing...");
+                isInGame = true;
+                Console.WriteLine("");
+            }
+            Thread.Sleep(500);
+        }
+        
+    }
+
 }
 
